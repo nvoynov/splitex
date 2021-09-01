f702e3535c7faf6de8ea425d6daa38c5164cbbd0
Nikolay Voynov <nvoynov@gmail.com>
2021-09-01 22:06:29 +0300

    added TODO

---
+++ TODO.md
@@ -0,0 +1,6 @@
+% TODO
+
+1. `hadn, wouldn` unfold contractions `had not, would not`
+2. `Name` skip words from uppercase letter?
+3. `delusion	3	delusions:1, delusion:2` skip form when it equal to lemma
+4. ask thor support about `"*.txt"` and use `'*.txt'` instead
--- lib/splitex/cli.rb
+++ lib/splitex/cli.rb
@@ -7,6 +7,12 @@ module Splitex
     include Thor::Actions
     namespace :splitex
 
+    no_commands {
+      def exit_on_failure?
+        true
+      end
+    }
+
     desc "version", "Show Worklog version"
     def version
       puts "Splitex v#{Splitex::VERSION}"
@@ -16,6 +22,9 @@ module Splitex
     desc "words [FROM] [SKIP]", "Extract words FROM files [and skip words from SKIP]"
     def words(from = "*.*", skip = "")
       Printer.(Builder.(from, skip))
+    rescue StandardError => e
+      puts e.message
+      puts e.backtrace.inspect
     end
 
   end
